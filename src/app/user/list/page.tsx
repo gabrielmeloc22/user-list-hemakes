4e94e7c6a9d1569ef6f0c48591c26d99e2e1ce76
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-08-31 14:40:09 -0300

    feat: add search input logic on list page

--- src/app/user/list/page.tsx
+++ src/app/user/list/page.tsx
@@ -2,12 +2,12 @@
 
 import { Avatar } from "@/components/Avatar";
 import { useUsers } from "@/hooks/useUsers";
+import { User } from "@/types/user";
 import { DotsThreeVertical, PencilSimple, Plus, TrashSimple } from "@phosphor-icons/react";
+import { fuzzyFilter } from "fuzzbunny";
 import NextLink from "next/link";
 import { useEffect, useState } from "react";
-import { UserSearch } from "../components/UserSearch";
-import { User } from "@/types/user";
-import { fuzzyFilter } from "fuzzbunny";
+import { UserSearchInput } from "../components/UserSearchInput";
 
 enum UserRoles {
   developer = "Developer",
@@ -17,7 +17,7 @@ enum UserRoles {
 }
 
 export default function UserListPage() {
-  const { users, removeOne } = useUsers();
+  const { users, deleteOne } = useUsers();
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState<User[]>();
 
@@ -42,82 +42,86 @@ export default function UserListPage() {
         </NextLink>
       </header>
       <main>
-        <UserSearch setSearchValue={setSearchValue} />
-        <table className="table">
-          <thead>
-            <tr>
-              <th>
-                <input type="checkbox" className="checkbox border-2" disabled={users?.length === 0}></input>
-              </th>
-              <th>Name</th>
-              <th>Company</th>
-              <th>Role</th>
-              <th>Verified</th>
-              <th>Status</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody className="[& td]">
-            {userTableData?.map(({ id, company, name, role, image, status, verified }) => (
-              <tr key={id}>
-                <td>
-                  <input type="checkbox" className="checkbox border-2" />
-                </td>
-                <td>
-                  <div className="flex gap-2 items-center font-semibold">
-                    <Avatar image={image} size="sm" circle />
-                    {name}
-                  </div>
-                </td>
-                <td>{company}</td>
-                <td>{UserRoles[role]}</td>
-                <td>{verified ? "Yes" : "No"}</td>
-                <td>
-                  <div
-                    className={`badge ${
-                      status === "active"
-                        ? "badge-success"
-                        : status === "banned"
-                        ? "badge-error"
-                        : "badge-neutral"
-                    } font-bold rounded-md px-3 py-1`}
-                  >
-                    {status}
-                  </div>
-                </td>
-                <td className="w-1">
-                  <div className="dropdown dropdown-end">
-                    <label className="hover:cursor-pointer" tabIndex={0}>
-                      <DotsThreeVertical size={24} />
-                    </label>
-                    <ul
-                      tabIndex={0}
-                      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-                    >
-                      <li>
-                        <button>
-                          <PencilSimple />
-                          Edit
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          onClick={() => {
-                            removeOne(id);
-                          }}
-                        >
-                          <TrashSimple />
-                          Remove
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                  <button type="button"></button>
-                </td>
+        <UserSearchInput setSearchValue={setSearchValue} />
+        <div className="max-sm:overflow-x-scroll min-w-[200px]">
+          <table className="table max-md:table-sm max-[375px]:table-xs">
+            <thead>
+              <tr>
+                <th>
+                  <input type="checkbox" className="checkbox border-2" disabled={users?.length === 0}></input>
+                </th>
+                <th>Name</th>
+                <th>Company</th>
+                <th>Role</th>
+                <th>Verified</th>
+                <th>Status</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {userTableData?.map(({ id, company, name, role, image, status, verified }) => (
+                <tr key={id}>
+                  <td>
+                    <input type="checkbox" className="checkbox border-2" />
+                  </td>
+                  <td>
+                    <div className="flex gap-2 items-center font-semibold">
+                      <Avatar image={image} size="sm" circle />
+                      {name}
+                    </div>
+                  </td>
+                  <td>{company}</td>
+                  <td>{UserRoles[role]}</td>
+                  <td>{verified ? "Yes" : "No"}</td>
+                  <td>
+                    <div
+                      className={`badge ${
+                        status === "active"
+                          ? "badge-success"
+                          : status === "banned"
+                          ? "badge-error"
+                          : "badge-info"
+                      } font-bold rounded-md px-3 py-1`}
+                    >
+                      {status}
+                    </div>
+                  </td>
+                  <td className="w-1">
+                    <div className="dropdown dropdown-end">
+                      <label className="hover:cursor-pointer" tabIndex={0}>
+                        <DotsThreeVertical size={24} />
+                      </label>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                      >
+                        <NextLink href={`edit/${id}`}>
+                          <li>
+                            <button>
+                              <PencilSimple />
+                              Edit
+                            </button>
+                          </li>
+                        </NextLink>
+                        <li>
+                          <button
+                            onClick={() => {
+                              deleteOne(id);
+                            }}
+                          >
+                            <TrashSimple />
+                            Remove
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                    <button type="button"></button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   );
